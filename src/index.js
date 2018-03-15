57c517a6e9ea9151980135ed8af9e7c33d45354e
Diego Rodrigues Vieira <diegorodrigues92@gmail.com>
2018-03-15 14:20:45 -0300

    Add DropZone Wrapper

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import Checkbox from './lib/Checkbox';
+import DropZone from './lib/DropZone';
 import RadioGroup from './lib/RadioGroup';
 import Select from './lib/Select';
 import TextField from './lib/TextField';
 import VirtualizedSelect from './lib/VirtualizedSelect';
 
-export { TextField, Select, Checkbox, RadioGroup, VirtualizedSelect };
+export { Checkbox, DropZone, RadioGroup, Select, TextField, VirtualizedSelect };
---
+++ src/lib/DropZone.js
@@ -0,0 +1,33 @@
+import { FormHelperText } from 'material-ui';
+import { XDropZone } from 'xbrain-react-components';
+import PropTypes from 'prop-types';
+import React, { Fragment } from 'react';
+
+const DropZoneWithReduxForm = ({
+  input: { onChange, onDrop, ...inputProps },
+  meta: { touched, error },
+  ...props
+}) => {
+  const hasError = Boolean(touched && error);
+
+  return (
+    <Fragment>
+      <XDropZone
+        onChange={(acceptedFiles) => {
+          onChange(acceptedFiles);
+        }}
+        error={hasError}
+        {...inputProps}
+        {...props}
+      />
+      {hasError && <FormHelperText error>{error}</FormHelperText>}
+    </Fragment>
+  );
+};
+
+DropZoneWithReduxForm.propTypes = {
+  input: PropTypes.object.isRequired,
+  meta: PropTypes.object.isRequired,
+};
+
+export default DropZoneWithReduxForm;
