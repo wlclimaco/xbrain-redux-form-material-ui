81dfbf57e21f3af9a1933fd34b02d399c6b1032a
Gabriel Ferraz <gabrielferraz@xbrain.com.br>
2019-01-14 13:55:02 -0200

    Remove logic for multiple select.

--- src/lib/VirtualizedSelect.js
+++ src/lib/VirtualizedSelect.js
@@ -1,48 +1,22 @@
-import { XVirtualizedSelect } from 'xbrain-react-components';
+// import { XVirtualizedSelect } from 'xbrain-react-components';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import PropTypes from 'prop-types';
 import React, { Fragment } from 'react';
+import XVirtualizedSelect from './Select';
 
 const ReactSelectWithReduxForm = ({
-  input: {
-    onChange, value, onBlur, ...inputProps
-  },
-  multiple,
-  simpleValue,
+  input,
   meta: { touched, error },
   ...props
 }) => {
-  let newValue = value;
-
-  if (multiple && simpleValue) {
-    if (typeof value === 'string') {
-      if (value) {
-        newValue = value.split(',');
-        newValue = newValue.map((item) => {
-          if (!isNaN(item)) {
-            return parseInt(item, 10);
-          }
-          return item;
-        });
-      } else {
-        newValue = [];
-      }
-    }
-  }
-
   const hasError = Boolean(touched && error);
 
   return (
     <Fragment>
       <XVirtualizedSelect
-        multiple={multiple}
-        value={newValue}
-        onChange={onChange}
-        onBlur={() => onBlur(newValue)}
         creatable={false}
         error={hasError}
-        simpleValue={simpleValue}
-        {...inputProps}
+        {...input}
         {...props}
       />
       {hasError && (
@@ -54,16 +28,9 @@ const ReactSelectWithReduxForm = ({
   );
 };
 
-ReactSelectWithReduxForm.defaultProps = {
-  multiple: false,
-  simpleValue: true,
-};
-
 ReactSelectWithReduxForm.propTypes = {
   input: PropTypes.object.isRequired,
   meta: PropTypes.object.isRequired,
-  multiple: PropTypes.bool,
-  simpleValue: PropTypes.bool,
 };
 
 export default ReactSelectWithReduxForm;
